585d91069bf6e79c9b18b7c5249e63043e3ec87c
denisbubykin <denis-bubykin@yandex.ru>
2023-02-09 14:12:26 +0300

    Added model, controller, tests

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/films")
+@Slf4j
+public class FilmController {
+
+    Map<Long, Film> films = new HashMap<>();
+
+    @PostMapping()
+    public Film addFilm(@Valid @RequestBody Film film) {
+        if(film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn(film.getReleaseDate().toString());
+            throw new ValidationException("Release date can't be before 28 dec. 1895 года");
+        }
+        log.debug(film.toString());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping()
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        if(film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn(film.getReleaseDate().toString());
+            throw new ValidationException("Release date can't be before 28 dec. 1895 года");
+        }
+        log.debug(film.toString());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @GetMapping()
+    public List<Film> getAllFilms() {
+        return films.values().stream().toList();
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+@Builder
+
+public class Film {
+
+    private long id;
+    @NotEmpty
+    private final String name;
+    private final String description;
+    @NotNull
+    private final LocalDate releaseDate;
+    @NotNull
+    private final Duration duration;
+
+
+}
