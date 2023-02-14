500b4f6de47ee2b65fd8c68847e856222df4101f
denisbubykin <denis-bubykin@yandex.ru>
2023-02-14 22:29:50 +0300

    Correct User#60

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -30,7 +30,7 @@ public class FilmController {
         if (isValid(film)) {
             film.setId(nextId);
             films.put(getNextId(), film);
-            log.info("Create film with id = {}", film.getId());
+            log.info("POST /films request received");
         } else {
             throw new ValidationException("Create film is not valid");
         }
