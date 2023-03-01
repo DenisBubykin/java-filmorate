f7ba59f5c2daf4aaaabf6cbe66ba678306da95e7
denisbubykin <denis-bubykin@yandex.ru>
2023-03-01 18:53:04 +0300

    Commit #13

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,98 +1,68 @@
 package ru.yandex.practicum.filmorate.controller;
 
-
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.time.LocalDate;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-public class FilmControllerTest {
-    @Autowired
-    private FilmController fController;
-    @Autowired
-    private FilmStorage filmStorage;
-    private static String name;
-    private static String description;
-    private static LocalDate date;
-    private static Integer duration;
-    private static Set<Long> likes;
-
+class FilmControllerTest {
+    private Film film;
+    private static Validator validator;
 
     @BeforeAll
-    public static void createFields() {
-        name = "name";
-        description = "description";
-        date = LocalDate.of(1999, 10, 5);
-        duration = 100;
+    public static void init() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        film = new Film();
+        film.setName("name");
+        film.setDescription("description");
+        film.setReleaseDate(LocalDate.of(2022, 01, 01));
+        film.setDuration(90);
     }
 
     @Test
-    void testCreateFilmWhenNameIsNotEmpty(){
-        filmStorage.getFilms().forEach(film -> filmStorage.delete(film));
-        Film film = new Film(name, description, date, duration, likes);
-        assertDoesNotThrow(() -> fController.create(film));
-        assertEquals(film, fController.getFilms().get(0));
-
+    void nameValidationTest() {
+        film.setName("");
+        Set<ConstraintViolation<Film>> validate = validator.validate(film);
+        Set<String> errorMessages = validate.stream().map(ConstraintViolation::getMessage).collect(Collectors.toSet());
+        assertEquals(1, errorMessages.size());
     }
 
     @Test
-    void testCreateFilmWhenNameIsEmpty(){
-        Film film = new Film("", description, date, duration, likes);
-        assertThrows(ValidationException.class, () -> fController.create(film));
+    void descriptionValidationTest() {
+        film.setDescription("111111111111111111111111111111111111111111111111111111111111111111111111111111111111111" +
+                "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111" +
+                "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+        Set<ConstraintViolation<Film>> validate = validator.validate(film);
+        Set<String> errorMessages = validate.stream().map(ConstraintViolation::getMessage).collect(Collectors.toSet());
+        assertEquals(1, errorMessages.size());
     }
 
     @Test
-    void testIsValidWhenDescriptionMoreThen() {
-        String line = "qwertyuiopasdftfftffufgu";
-        Film film = new Film(name, line.repeat(10), date, duration, likes);
-        assertThrows(ValidationException.class, () -> fController.create(film));
+    void dateValidationTest() {
+        film.setReleaseDate(LocalDate.of(1895, 12, 27));
+        Set<ConstraintViolation<Film>> validate = validator.validate(film);
+        Set<String> errorMessages = validate.stream().map(ConstraintViolation::getMessage).collect(Collectors.toSet());
+        assertFalse(errorMessages.contains("дата релиза — не раньше 28 декабря 1895 года"));
     }
 
     @Test
-    void testIsValidWhenRealiseDate() {
-        LocalDate date = LocalDate.of(1700, 10, 5);
-        Film film = new Film(name, description, date, duration, likes);
-        assertThrows(ValidationException.class, () -> fController.create(film));
-    }
-
-    @Test
-    void testIsValidWhenDuration(){
-        Film film = new Film(2, name, description, date, -1, likes);
-        assertThrows(ValidationException.class, () -> fController.create(film));
-
-    }
-
-    @Test
-    void ShouldValidateId(){
-        Film film1 = new Film(1, name, description, date, 1, likes);
-        Film film2= new Film(2, name, description, date, 2, likes);
-        Film film3= new Film(3, name, description, date, 3, likes);
-        try {
-            fController.create(film1);
-            fController.create(film2);
-            fController.create(film3);
-        } catch (ValidationException e) {
-            System.out.println(e.getMessage());
-        }
-        Film updateFilm1 = new Film(1, name, description, date, 100, likes);
-        Film updateFilm2 = new Film(0, name, description, date, 100, likes);
-        Film updateFilm3 = new Film(-1, name, description, date, 100, likes);
-        try {
-            fController.update(updateFilm1);
-        } catch (ValidationException e) {
-            System.out.println(e.getMessage());
-        }
-        assertEquals(updateFilm1, fController.getFilms().get(0));
-        assertThrows(ValidationException.class, () -> fController.update(updateFilm2));
-        assertThrows(ValidationException.class, () -> fController.update(updateFilm3));
+    void durationValidationTest() {
+        film.setDuration(0);
+        Set<ConstraintViolation<Film>> validate = validator.validate(film);
+        Set<String> errorMessages = validate.stream().map(ConstraintViolation::getMessage).collect(Collectors.toSet());
+        assertEquals(1, errorMessages.size());
     }
 }
