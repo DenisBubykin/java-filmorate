69a55f239fe298c3620e23d007fd481c11199525
denisbubykin <denis-bubykin@yandex.ru>
2023-03-06 00:24:30 +0300

    Commit #31

--- src/main/java/ru/yandex/practicum/filmorate/validator/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validator/UserValidator.java
@@ -40,13 +40,13 @@ public class UserValidator {
 
     public static void isValidIdUsers(Long id) {
         if (id < 0) {
-            throw new NotFoundException("Id пользователя {} отрицательный");
+            throw new NotFoundException(String.format("Id пользователя {} отрицательный", id));
         }
     }
 
     public static void isUserByUsers(List<User> users, User user) {
         if (!users.contains(user)) {
-            throw new NotFoundException("Пользователь № %d не найден");
+            throw new NotFoundException(String.format("Пользователь № %d не найден", user.getId()));
         }
     }
 }
